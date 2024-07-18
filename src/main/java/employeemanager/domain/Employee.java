090e7992c19082bf51a38fd68ff3f193aeba0d02
VitorApolonio <vitorapolonio74@gmail.com>
2024-07-18 19:32:39 -0300

    Add toString() to Employee class

--- src/main/java/employeemanager/domain/Employee.java
+++ src/main/java/employeemanager/domain/Employee.java
@@ -45,4 +45,9 @@ public class Employee {
     public int hashCode() {
         return Objects.hashCode(name);
     }
+
+    @Override
+    public String toString() {
+        return name + ", " + jobTitle + ", $" + salary;
+    }
 }
