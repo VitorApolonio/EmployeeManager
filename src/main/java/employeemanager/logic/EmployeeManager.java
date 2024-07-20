ba1b10ba38b1ff039a2943a99d8defff4b81aa2b
VitorApolonio <vitorapolonio74@gmail.com>
2024-07-20 19:03:11 -0300

    Create EmployeeManager save method
    Also creates a test for the method and changes the Map implementation
    to LinkedHashMap, for a predictable employee order when saving.

--- src/main/java/employeemanager/logic/EmployeeManager.java
+++ src/main/java/employeemanager/logic/EmployeeManager.java
@@ -2,14 +2,17 @@ package employeemanager.logic;
 
 import employeemanager.domain.Employee;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class EmployeeManager {
     private final Map<String, Employee> employees;
 
     public EmployeeManager() {
-        employees = new HashMap<>();
+        employees = new LinkedHashMap<>();
     }
 
     public void add(Employee employee) {
@@ -35,5 +38,20 @@ public class EmployeeManager {
         }
 
         return false;
+    }
+
+    public boolean save(String filename) {
+        try (FileWriter writer = new FileWriter(filename)) {
+            for (Employee employee : employees.values()) {
+                String employeeData = employee.getName() + "," + employee.getJobTitle() + "," + employee.getSalary();
+
+                writer.write(employeeData);
+                writer.write(System.lineSeparator());
+            }
+
+            return true;
+        } catch (IOException e) {
+            return false;
+        }
     }
 }
--- src/test/java/employeemanager/logic/EmployeeManagerTest.java
+++ src/test/java/employeemanager/logic/EmployeeManagerTest.java
@@ -4,8 +4,12 @@ import employeemanager.domain.Employee;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
 import java.math.BigDecimal;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Map;
+import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -68,5 +72,34 @@ class EmployeeManagerTest {
 
         Assertions.assertTrue(manager.hasEmployee("John Doe"));
         Assertions.assertFalse(manager.hasEmployee("John Smith"));
+    }
+
+    @Test
+    void save_writes_to_file() throws IOException {
+        EmployeeManager manager = new EmployeeManager();
+        Employee john = new Employee("John Doe", "HR Manager", new BigDecimal("8000.00"));
+        Employee alice = new Employee("Alice", "HR Supervisor", new BigDecimal("6000.00"));
+        Employee bob = new Employee("Bob", "Sales Manager", new BigDecimal("12000.00"));
+
+        manager.add(john);
+        manager.add(alice);
+        manager.add(bob);
+
+        Random rand = new Random();
+        String fileName = "_savetest_" + rand.nextInt(800) + ".txt";
+
+        manager.save(fileName);
+
+        String expected = """
+                John Doe,HR Manager,8000.00
+                Alice,HR Supervisor,6000.00
+                Bob,Sales Manager,12000.00
+                """.replaceAll("\n", System.lineSeparator());
+
+        String fileContent = Files.readString(Path.of(fileName));
+
+        Assertions.assertEquals(expected, fileContent);
+
+        Files.deleteIfExists(Path.of(fileName));
     }
 }
